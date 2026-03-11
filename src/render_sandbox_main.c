0e286fcea2835de435ba36ecdad9740b386cb8a2
kyanagis <kyanagis42@gmail.com>
2026-03-11 16:37:53 +0900

    add: 簡易エントリポイントを追加し、パース処理を飛ばし描画とraycastの実装を行うためのmain関数を追加

---
+++ src/render_sandbox_main.c
@@ -0,0 +1,190 @@
+
+
+#include "cub3d.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+/*
+parser を通さずに描画と raycast を実装するための簡易エントリポイント.
+maps/good/library.cub 相当の t_config を手動で組み立てる.
+
+F 139,99,58
+C 171,154,137
+
+NO texture/direction/north.xpm
+SO texture/direction/south.xpm
+WE texture/direction/west.xpm
+EA texture/direction/east.xpm
+
+1111111111111111111
+1001001001001001001
+1001001001001001001
+1001001001001001001
+1001001000001001001
+100000000S000000001
+1000011111111000001
+1111000000000001111
+1000000101010000001
+1111111111111111111
+
+
+*/
+
+
+static void	free_sandbox_config(t_config *config){
+	int	i;
+
+	i = 0;
+	while (i < TEX_COUNT){
+		free(config->tex.path[i]);
+		config->tex.path[i] = NULL;
+		i++;
+	}
+	if (config->map.grid == NULL)
+		return ;
+	i = 0;
+	while (i < config->map.height){
+		free(config->map.grid[i]);
+		i++;
+	}
+	free(config->map.grid);
+	config->map.grid = NULL;
+}
+
+static bool	init_tex_path(t_tex_path *tex){
+	tex->path[TEX_NO] = strdup("texture/direction/north.xpm");
+	tex->path[TEX_SO] = strdup("texture/direction/south.xpm");
+	tex->path[TEX_WE] = strdup("texture/direction/west.xpm");
+	tex->path[TEX_EA] = strdup("texture/direction/east.xpm");
+	return tex->path[TEX_NO] != NULL && tex->path[TEX_SO] != NULL
+		&& tex->path[TEX_WE] != NULL && tex->path[TEX_EA] != NULL;
+}
+
+static inline void	add_value(t_rgb *color){
+	color->value = (color->r << 16) | (color->g << 8) | color->b;
+}
+
+static void	init_color(t_rgb *floor_color, t_rgb *ceiling_color){
+	floor_color->r = 139;
+	floor_color->g = 99;
+	floor_color->b = 58;
+	floor_color->is_set = true;
+	add_value(floor_color);
+	ceiling_color->r = 171;
+	ceiling_color->g = 154;
+	ceiling_color->b = 137;
+	ceiling_color->is_set = true;
+	add_value(ceiling_color);
+}
+
+static const char	*g_map_src[] = {
+	"1111111111111111111",
+	"1001001001001001001",
+	"1001001001001001001",
+	"1001001001001001001",
+	"1001001000001001001",
+	"100000000S000000001",
+	"1000011111111000001",
+	"1111000000000001111",
+	"1000000101010000001",
+	"1111111111111111111"
+};
+
+static inline int	get_map_height(void){
+	return (int)(sizeof(g_map_src) / sizeof(g_map_src[0]));
+}
+
+static int	get_map_width(void){
+	int		i;
+	int		width;
+	size_t	row_len;
+
+	i = 0;
+	width = 0;
+	while (i < get_map_height()){
+		row_len = strlen(g_map_src[i]);
+		if ((int)row_len > width)
+			width = (int)row_len;
+		i++;
+	}
+	return width;
+}
+
+static bool	get_spawn_dir(char cell, t_dir *dir)
+{
+	if (cell == 'N')
+		*dir = NORTH;
+	else if (cell == 'S')
+		*dir = SOUTH;
+	else if (cell == 'E')
+		*dir = EAST;
+	else if (cell == 'W')
+		*dir = WEST;
+	else
+		return false;
+	return true;
+}
+
+static bool	init_map_and_spawn(t_map *map, t_spawn *spawn){
+	t_dir	dir;
+	int		x;
+	int		y;
+	bool	found_spawn;
+
+	map->height = get_map_height();
+	map->width = get_map_width();
+	map->grid = calloc((size_t)map->height + 1, sizeof(*map->grid));
+	if (map->grid == NULL)
+		return false;
+	found_spawn = false;
+	y = 0;
+	while (y < map->height){
+		map->grid[y] = strdup(g_map_src[y]);
+		if (map->grid[y] == NULL)
+			return false;
+		x = 0;
+		while (map->grid[y][x] != '\0'){
+			if (get_spawn_dir(map->grid[y][x], &dir)){
+				if (found_spawn)
+					return false;
+				found_spawn = true;
+				spawn->row = y;
+				spawn->col = x;
+				spawn->dir = dir;
+				map->grid[y][x] = '0';
+			}
+			x++;
+		}
+		y++;
+	}
+	return found_spawn;
+}
+
+static bool	init_sandbox_config(t_config *config){
+	memset(config, 0, sizeof(*config));
+	if (!init_tex_path(&config->tex)){
+		free_sandbox_config(config);
+		return false;
+	}
+	init_color(&config->floor_color, &config->ceiling_color);
+	if (!init_map_and_spawn(&config->map, &config->spawn)){
+		free_sandbox_config(config);
+		return false;
+	}
+	return true;
+}
+
+int	main(void){
+	t_config	config;
+
+	if (!init_sandbox_config(&config))
+		return 1;
+
+	
+
+	free_sandbox_config(&config);
+	return 0;
+}
