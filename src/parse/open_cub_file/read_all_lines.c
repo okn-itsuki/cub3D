8f8ae1640a13c5c4c50c87e3fc02cd9e6f8148fa
iokuno <okuno.4115@gmail.com>
2026-03-14 16:40:01 +0900

    ノーム直した

--- src/parse/open_cub_file/read_all_lines.c
+++ src/parse/open_cub_file/read_all_lines.c
@@ -6,40 +6,29 @@
 /*   By: iokuno <iokuno@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/14 14:26:54 by iokuno            #+#    #+#             */
-/*   Updated: 2026/03/14 14:44:55 by iokuno           ###   ########.fr       */
+/*   Updated: 2026/03/14 16:39:35 by iokuno           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdbool.h>
 
-char	**read_all_lines(int fd)
+// 概要 : ファイルの中身をメモリに書き込む
+// 返り値 : ファイルを1行ずつ保有する二次元配列（ダブルポインタ）
+// エラー処理 :
+const char	**read_all_lines(int fd)
 {
-	char	**lines;
-	char	*next_line;
-	char	**new_lines;
-	size_t	capacity;
-	size_t	count;
+	const char	**line;
+	int			i;
 
-	capacity = 16;
-	count = 0;
-	lines = ft_xmalloc(sizeof(char *) * (capacity + 1));
+	i = 0;
 	while (true)
 	{
-		next_line = get_next_line(fd);
-		if (next_line == NULL)
-			break;
-		if (count == capacity)
-		{
-			capacity *= 2;
-			new_lines = ft_xmalloc(sizeof(char *) * (capacity + 1));
-			ft_memcpy(new_lines, lines, sizeof(char *) * count);
-			free(lines);
-			lines = new_lines;
-		}
-		lines[count] = next_line;
-		count++;
+		line[i] = get_next_line(fd);
+		if (line[i] == NULL)
+			break ;
+		i++;
 	}
-	lines[count] = NULL;
-	return (lines);
+// TODO : issue #5
+	return (line);
 }
