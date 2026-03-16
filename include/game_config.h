0eafb63cc7ca20c90d8a8cd4007abd251c544059
kyanagis <kyanagis42@gmail.com>
2026-03-16 15:22:34 +0900

    add: game_config.h

---
+++ include/game_config.h
@@ -0,0 +1,49 @@
+#ifndef GAME_CONFIG_H
+#define GAME_CONFIG_H
+
+
+
+
+// linux,mac対応のゴリ押しkeycode
+#ifndef KEYCODES_H
+# define KEYCODES_H
+
+
+# if defined(__linux__)
+#  define PLATFORM_LINUX 1
+# elif defined(__APPLE__) && defined(__MACH__)
+#  define PLATFORM_MACOS 1
+# endif
+
+
+# if defined(PLATFORM_LINUX)
+
+#  include <X11/keysym.h>
+
+#  define KEY_ESC       XK_Escape
+#  define KEY_LEFT      XK_Left
+#  define KEY_RIGHT     XK_Right
+#  define KEY_UP        XK_Up
+#  define KEY_DOWN      XK_Down
+#  define KEY_W         XK_w
+#  define KEY_A         XK_a
+#  define KEY_S         XK_s
+#  define KEY_D         XK_d
+
+# elif defined(PLATFORM_MACOS)
+
+#  define KEY_ESC       53
+#  define KEY_LEFT      123
+#  define KEY_RIGHT     124
+#  define KEY_DOWN      125
+#  define KEY_UP        126
+#  define KEY_A         0
+#  define KEY_S         1
+#  define KEY_D         2
+#  define KEY_W         13
+
+# endif
+
+#endif
+
+#endif
