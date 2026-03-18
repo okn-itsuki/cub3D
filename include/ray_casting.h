7e5b4730f7f821b6f0d78a5920cf7488f7eedfaa
kyanagis <kyanagis42@gmail.com>
2026-03-18 20:10:00 +0900

    add: ray-castingの宣言の追加

---
+++ include/ray_casting.h
@@ -0,0 +1,14 @@
+#ifndef RAY_CASTING_H
+#define RAY_CASTING_H
+
+#include "cub3d.h"
+
+void	cast_ray(t_ray *ray, const t_player *player, const t_map *map,
+			int col);
+void	render_column(t_render *render, const t_assets *assets);
+void	render_frame(t_game *game);
+void	update_player(t_player *player, const t_input *input,
+			const t_map *map, double dt);
+int		game_loop_tick(t_game *game);
+
+#endif
