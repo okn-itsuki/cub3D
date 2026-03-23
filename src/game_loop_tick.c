121d17a749515d2731292a131769d0073a373098
kyanagis <kyanagis42@gmail.com>
2026-03-23 13:31:15 +0900

    fix: μsの変換式のマジックナンバーを定数化

--- src/game_loop_tick.c
+++ src/game_loop_tick.c
@@ -3,10 +3,12 @@
 #include <sys/time.h>
 #include <stdlib.h>
 
+#define US_SEC 1e-6
+
 static uint64_t	get_current_time_us(void)
 {
 	struct timeval	tv;
 
 	gettimeofday(&tv, NULL);
-	return ((uint64_t)tv.tv_sec * 1000000 + (uint64_t)tv.tv_usec);
+	return ((uint64_t)tv.tv_sec * US_SEC + (uint64_t)tv.tv_usec);
 }
