d62ad3600384639c4b11991106ba9e535231bb6e
okn-itsuki <okuno.4115@gamil.com>
2026-04-03 14:45:43 +0900

    config を freeする関数を追加

--- src/parse/free_config.c
+++ src/parse/free_config.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   free_config.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: iokuno <iokuno@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*   By: okunoitsuki <okunoitsuki@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/31 00:00:00 by iokuno            #+#    #+#             */
-/*   Updated: 2026/03/31 00:00:00 by iokuno           ###   ########.fr       */
+/*   Updated: 2026/04/03 11:55:24 by okunoitsuki      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,26 @@
 #include "cub_config.h"
 #include "parse.h"
 
-static void	free_tex_paths(t_tex_path *tex);
-static void	free_map_grid(t_map *map);
+static void free_tex_paths(t_tex_path *tex);
+static void free_map_grid(t_map *map);
 
-// 概要 : t_config が保持するヒープ領域をすべて解放する
-// 参照で変更された値 :
-//   config->tex.path[] を NULL にする
-//   config->map.grid を NULL にする
-// メモリ開放義務 : parse_cub 成功後、呼び出し元が責任を持って呼ぶ
-void	free_config(t_config *config)
+/*
+ * 概要 : t_config が保持するヒープ領域をすべて解放する
+ * 参照で変更された値 :
+ *   config->tex.path[] を NULL にする
+ *   config->map.grid を NULL にする
+ * メモリ開放義務 : parse_cub 成功後、呼び出し元が責任を持って呼ぶ
+ */
+void free_config(t_config *config)
 {
 	free_tex_paths(&config->tex);
 	free_map_grid(&config->map);
 }
 
 // 概要 : テクスチャパス配列を解放する
-static void	free_tex_paths(t_tex_path *tex)
+static void free_tex_paths(t_tex_path *tex)
 {
-	int	i;
+	int i;
 
 	i = 0;
 	while (i < TEX_COUNT)
@@ -43,12 +45,12 @@ static void	free_tex_paths(t_tex_path *tex)
 }
 
 // 概要 : マップグリッドの各行と配列本体を解放する
-static void	free_map_grid(t_map *map)
+static void free_map_grid(t_map *map)
 {
-	int	i;
+	int i;
 
 	if (map->grid == NULL)
-		return ;
+		return;
 	i = 0;
 	while (i < map->height)
 	{
