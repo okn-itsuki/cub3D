1acd24ecd299433d3c33f1a0495cfd5ac921b99c
oitsuki <okuno.4115@gmail.com>
2026-04-17 09:42:40 +0900

    add mainから呼び出す最初の関数の大まかな形

---
+++ include/parse_line_type.h
@@ -0,0 +1,13 @@
+#ifndef PARSE_LINE_TYPE_H
+#define PARSE_LINE_TYPE_H
+
+typedef enum e_parse_line_type
+{
+	LINE_EMPTY,
+	LINE_TEXTURE,
+	LINE_COLOR,
+	LINE_MAP,
+	LINE_INVALID
+}	t_parse_line_type;
+
+#endif
