d24418fdabda0eb395d9610f7a6ea7b5a064dd51
oitsuki <okuno.4115@gmail.com>
2026-04-17 11:53:43 +0900

    コメントつけたよ

--- include/parse_line_type.h
+++ include/parse_line_type.h
@@ -1,6 +1,17 @@
 #ifndef PARSE_LINE_TYPE_H
 #define PARSE_LINE_TYPE_H
 
+
+/**
+ * @file parse_line_type.h
+ * @brief その行がどの要素の行なのか
+
+ * @param LINE_EMPTY NULL
+ * @param LINE_TEXTURE NO, SO, WE, EA
+ * @param LINE_COLOR F or C
+ * @param LINE_MAP 1 0 N 
+ * @param LINE_INVALID 無効な値
+ */
 typedef enum e_parse_line_type
 {
 	LINE_EMPTY,
