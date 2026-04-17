28d8b2df3e27097611c91e5fcfb1bc48f3579639
oitsuki <okuno.4115@gmail.com>
2026-04-17 15:14:02 +0900

    parse texture と parse elementsの実装 t_sys_err を t_excepionに変更

---
+++ src/parse/parse_elements.c
@@ -0,0 +1,56 @@
+/**
+ * @file parse_elements.c
+ * @brief ヘッダ行の振り分けと必須要素の充足確認
+ */
+#include "parse.h"
+
+/**
+ * @brief ヘッダ行 1 行を識別して適切な解析関数へ振り分けます。
+ *
+ * @param line 解析対象のヘッダ行。
+ * @param config 解析結果の格納先。
+ *
+ * @retval true 行の解析に成功した場合。
+ * @retval false 行種別不正または要素解析に失敗した場合。
+ */
+t_excepion	parse_header_line(const char *line, t_config *config)
+{
+	t_parse_line_type	type;
+	t_excepion			state;
+
+	type = parse_get_line_type(line);
+	if (type == LINE_TEXTURE)
+		return (parse_texture_line(line, config));
+// TODO : color parse 実装
+	if (type == LINE_COLOR)
+		return (parse_color_line(line, config));
+// TODO : 例外内容増やす 
+	return (excepion_argment("invalid header line"));
+}
+
+/**
+ * @brief 必須ヘッダ 6 要素がすべて設定済みか判定します。
+ *
+ * @param config 判定対象の設定構造体。
+ *
+ * @retval true テクスチャ 4 枚と床・天井色が揃っている場合。
+ * @retval false いずれかが未設定の場合。
+ */
+t_excepion parse_all_headers_set(const t_config *config)
+{
+	int	index;
+
+	index = 0;
+	while (index < TEX_COUNT)
+	{
+// TODO : issue33 
+		if (config->tex.path[index] == NULL)
+			return (excepiom_texture("Required texture images are missing.\n"));
+		index++;
+	}
+	if (!config->floor_color.is_set )
+		return (excepiom_texture("Floor color has not been specified."));
+	if (!config->ceiling_color.is_set)
+		return (excepiom_texture("Ceiling color has not been specified."));
+	return (SUCCESS);
+}
--- src/parse/system_err_mes.c
+++ src/parse/system_err_mes.c
@@ -4,7 +4,7 @@
 #include "libft.h"
 #include "parse.h"
 
-t_system_err	open_file(const char *path, int *fd)
+t_excepion	open_file(const char *path, int *fd)
 {
 	*fd = open(path, O_RDONLY);
 	if (*fd < 0)
@@ -16,7 +16,7 @@ t_system_err	open_file(const char *path, int *fd)
 	return (SUCCESS);
 }
 
-t_system_err	read_file_buf(int fd,
+t_excepion	read_file_buf(int fd,
 	char *buf, size_t buf_size, ssize_t *read_size)
 {
 	*read_size = read(fd, buf, buf_size);
@@ -29,7 +29,7 @@ t_system_err	read_file_buf(int fd,
 	return (SUCCESS);
 }
 
-t_system_err	close_file(int fd)
+t_excepion	close_file(int fd)
 {
 	if (close(fd) < 0)
 	{
@@ -40,9 +40,22 @@ t_system_err	close_file(int fd)
 	return (SUCCESS);
 }
 
-t_system_err	malloc_err(void)
+// TODO : malloc_excepion に名前変更
+t_excepion	malloc_err(void)
 {
 	ft_putstr_fd("Error\n", STDERR_FILENO);
 	ft_putstr_fd("memory allocation failed\n", STDERR_FILENO);
-	return (MALLOC_ERR);
+	return (MALLOC_FAIL);
+}
+
+t_excepion excepiom_argment(const char *message)
+{
+	ft_putstr_fd((char *)message,STDERR_FILENO);
+	return (INVALID_STRING);
+}
+
+t_excepion excepiom_texture(const char *message)
+{
+	ft_putstr_fd((char *)message,STDERR_FILENO);
+	return (TEX_ERR);
 }
