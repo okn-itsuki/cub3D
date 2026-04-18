dda05714c66d0e8a831c5f525de9aef588b891a8
iokuno <okuno.4115@gmail.com>
2026-04-18 21:18:12 +0900

    enum 名前を変更に伴って

--- include/parse.h
+++ include/parse.h
@@ -2,23 +2,23 @@
 #define PARSE_H
 
 #include <stdbool.h>
-#include "parse_system_err.h"
+#include "parse_excepion.h"
 #include "cub_config.h"
 
 bool			parse_cub(const char *path, t_config *config);
 void			free_config(t_config *config);
 
 // read_file.c
-t_system_err	read_file_lines(const char *path, char ***ptr);
+t_excepion		read_file_lines(const char *path, char ***ptr);
 void			free_lines(char **lines);
 
 // split_lines.c
-t_system_err	split_lines(const char *content, char ***lines);
+t_excepion		split_lines(const char *content, char ***lines);
 
 // system_err_mes.c
-t_system_err	open_file(const char *path, int *fd);
-t_system_err	read_file_buf(int fd, char *buf, size_t buf_size, ssize_t *read_size);
-t_system_err	close_file(int fd);
-t_system_err	malloc_err(void);
+t_excepion		open_file(const char *path, int *fd);
+t_excepion		read_file_buf(int fd, char *buf, size_t buf_size, ssize_t *read_size);
+t_excepion		close_file(int fd);
+t_excepion		malloc_err(void);
 
 #endif
