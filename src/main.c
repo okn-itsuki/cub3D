aa37b5b732f2ebdd9e4114348d8a1aba8ac897ff
iokuno <okuno.4115@gmail.com>
2026-05-12 18:25:51 +0900

    integration main

--- src/main.c
+++ src/main.c
@@ -17,14 +17,15 @@
 int	main(int argc, char **argv)
 {
 	t_config	config;
+	t_excepion	state;
 
 	if (argc != 2)
 	{
 		fprintf(stderr, "Usage: %s <map.cub>\n", argv[0]);
 		return (1);
 	}
-	/* TODO: parse_cub(argv[1], &config) が実装されたらここで呼ぶ */
-	(void)config;
-	printf("file: %s\n", argv[1]);
-	return (0);
+	state = parse_cub(argv[1], &config);
+	if (state != SUCCESS)
+//TODO : render()
+	return (state);
 }
